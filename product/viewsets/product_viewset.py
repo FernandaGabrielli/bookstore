782d907146962666817760bb55f9c6237be54946
FernandaGabrielli <fernandagabriellidev@gmail.com>
2024-09-02 22:46:17 -0300

    Erro ao inicializar

---
+++ product/viewsets/product_viewset.py
@@ -0,0 +1,16 @@
+from rest_framework.viewsets import ModelViewSet
+
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import isAuthenticated
+
+from product.models import Product
+from product.serializers.product_serializer import ProductSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [isAuthenticated]
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        return Product.objects.all()
